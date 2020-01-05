1ef61991ce8ac33c185e84b7dd73d99ff32cb6e2
Zachary Elliott <contact@zell.io>
2020-01-05 00:24:43 +0000

    Strips useless package/ from branch names

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ function main
 {
     local package_name="$1"
     docker build --tag pkgbuild-builder:latest --file Dockerfile .
-    docker run --volume "$(pwd)":/mnt/return_target --rm --env package="${package_name}" pkgbuild-builder:latest
+    docker run --volume "$(pwd)":/mnt/return_target --rm --env package="${package_name#package/}" pkgbuild-builder:latest
 }
 
 main "$@"
